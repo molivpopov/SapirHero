0b1f2cb9c95111ed6caabe11742a8e0dd23f80f7
molivpopov <mitkop@mail.bg>
2021-04-20 14:04:54 +0300

    new readme

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::group(['middleware' => ['auth']], function () {
-    Route::get('/home', 'HomeController@index')
+    Route::get('/home', function (){
+        return redirect()->route('play');
+    })
         ->name('home');
 
     Route::get('/play', 'PlayController@index')
